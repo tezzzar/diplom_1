4810344785d12e6bb8ab0875d199fe73d725a85f
tezzzar <olexandr.borodavko@gmail.com>
2022-10-04 18:50:13 +0300

    fix slider arrows

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 $(".slider").slick({
   dots: true,
 });
+
+let dotsFix = document.querySelectorAll("button[role='tab']");
+dotsFix.forEach((item) => {
+  item.classList.add("slider-button-fix");
+});
+
+let slickDotsFix = document.querySelector(".slick-dots");
+slickDotsFix.classList.add("slick-dots-fix");
+
+// let slickText = document.querySelectorAll(".slider__item");
+// console.log(slickText);
