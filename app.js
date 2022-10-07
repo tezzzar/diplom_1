f573b2cb1fc48155414897de424c92b08882500a
tezzzar <olexandr.borodavko@gmail.com>
2022-10-07 14:24:38 +0300

    add second slider

--- app.js
+++ app.js
@@ -8,20 +8,33 @@ dotsFix.forEach((item) => {
   item.classList.add("slider-button-fix");
 });
 
-// let slickDotsFix = document.querySelector(".slick-dots");
-// slickDotsFix.classList.add("slick-dots-fix");
-
 let slickDotsFix = document.querySelectorAll(".slick-dots");
 slickDotsFix.forEach((item) => {
   item.classList.add("slick-dots-fix");
 });
 
-// let slickText = document.querySelectorAll(".slider__item");
-// console.log(slickText);
-
 $(".slider2").slick({
   dots: true,
   arrows: true,
   slidesToShow: 5,
+  slidesToScroll: 2,
+});
+
+$(".slider3").slick({
+  dots: false,
+  arrows: true,
+  slidesToShow: 5,
   slidesToScroll: 5,
+});
+
+$(".slider-dots .slider-dots__item").click(function () {
+  var $this = $(this);
+  let sliderDotsActive = document.querySelectorAll(".slider-dots__item");
+  sliderDotsActive.forEach((item) => {
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+    }
+    $(".slider3").slick("slickGoTo", $this.data("index"));
+    this.classList.add("active");
+  });
 });
