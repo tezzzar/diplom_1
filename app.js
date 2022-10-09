c2d20ad34dc21ba51ea87ef236d497d2224d3f03
tezzzar <olexandr.borodavko@gmail.com>
2022-10-09 20:05:03 +0300

    add footer

--- app.js
+++ app.js
@@ -64,3 +64,37 @@ const showPrev = () => {
 };
 btnPrev.addEventListener("click", showPrev);
 btnNext.addEventListener("click", showNext);
+
+// ------------Button - Top ---------------
+
+const btnUp = {
+  el: document.querySelector(".button-top"),
+  show() {
+    // удалим у кнопки класс btn-up_hide
+    this.el.classList.remove("btn-up_hide");
+  },
+  hide() {
+    // добавим к кнопке класс btn-up_hide
+    this.el.classList.add("btn-up_hide");
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener("scroll", () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 2600 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector(".button-top").onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+  },
+};
+
+btnUp.addEventListener();
